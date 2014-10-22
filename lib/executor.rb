f76571a418b0c54d0bad1fce4a06c8d820e63bbd
Janoo Fernandes <smoothclip@gmail.com>
2014-10-22 14:23:41 -0400

    Fix executor.list_all error

--- lib/executor.rb
+++ lib/executor.rb
@@ -9,7 +9,6 @@ module Crucible
       def execute_all
         results = {}
         self.class.tests.each do |test|
-          next if test == :BaseTest
           results[test] = {
             test_file: test,
             tests: Crucible::Tests.const_get(test).new(@client).execute
@@ -23,8 +22,7 @@ module Crucible
 
       def self.list_all
         list = {}
-        self.class.tests.each do |test|
-          next if test == :BaseTest
+        self.tests.each do |test|
           test_file = Crucible::Tests.const_get(test).new(nil)
           list[test] = {}
           Crucible::Tests::BaseTest::JSON_FIELDS.each {|field| list[test][field] = test_file.send(field)}
@@ -34,7 +32,7 @@ module Crucible
 
       def self.tests
         # sort test files by defined id field
-        Crucible::Tests.constants.grep(/Test$/).sort{|t1,t2| Crucible::Tests.const_get(t1).new(nil).id <=> Crucible::Tests.const_get(t2).new(nil).id }
+        Crucible::Tests.constants.grep(/Test$/).sort{|t1,t2| Crucible::Tests.const_get(t1).new(nil).id <=> Crucible::Tests.const_get(t2).new(nil).id } - [:BaseTest]
       end
 
     end

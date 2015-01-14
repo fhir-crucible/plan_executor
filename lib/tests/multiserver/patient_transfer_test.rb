2f5f3cf7d6dc03827473d9475729c8177aec9d4d
Andre Quina <aquina@mitre.org>
2015-01-14 16:58:07 -0500

    updated patient transfer multiserver test

--- lib/tests/multiserver/patient_transfer_test.rb
+++ lib/tests/multiserver/patient_transfer_test.rb
@@ -24,32 +24,54 @@ module Crucible
 
       test 'I01','Transfer Patient between servers without id' do
         metadata {
-          # links 'http://www.hl7.org/implement/standards/fhir/http.html#history'
-          # requires resource: "Patient", methods: ["create", "update", "delete"]
-          # validates resource: "Patient", methods: ["history-instance"]
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#read'
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#create'
+          requires resource: "Patient", methods: ["create", "read"]
+          validates resource: "Patient", methods: ["create", "read"]
         }
 
         @patient = @resources.example_patient
-        result = @client.create(@patient)
-        # assert the response was good on creation
-        read = @client.read(FHIR::Patient, result.resource.xmlId)
-        read_resource = read.resource
-        read_resource.xmlId = nil
-        create_result = @client2.create(read_resource)
-        read_client2 = @client2.read(FHIR::Patient, create_result.id)
 
+        client1_patient = create_and_read_patient(@patient, @client)
+
+        # clear the ID, there is a separate test to test preserved ids
+        client1_id = client1_patient.xmlId
+        client1_patient.xmlId = nil
+
+        client2_patient = create_and_read_patient(client1_patient, @client2)
+
+        mismatch = client1_patient.mismatch client2_patient, ['_id', 'xmlId', 'versionId', 'div', 'lastUpdated']
+        assert mismatch.empty?, "The transfered patient did not match the original for the following fields: #{mismatch}"
+
+        mismatch = client1_patient.mismatch client2_patient, ['_id', 'xmlId', 'versionId']
+        warning {assert mismatch.empty?, "The transfered patient did not match the original in the narrative section"}
+
+      end
+
+      def create_and_read_patient(patient, client)
+        result = client.create(patient)
         assert_response_ok result
-        assert false, "failing_string"
 
-        # check all this stuff with asserts
-
+        read = client.read(FHIR::Patient, result.id)
+        assert_response_ok read
+        read.resource
       end
 
       test 'I02','Transfer Patient between servers with id' do
+        metadata {
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#read'
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#create'
+          requires resource: "Patient", methods: ["create", "read"]
+          validates resource: "Patient", methods: ["create", "read"]
+        }
+
+        @patient = @resources.example_patient
+
+        client1_patient = create_and_read_patient(@patient, @client)
+        client2_patient = create_and_read_patient(client1_patient, @client2)
+        assert client1_patient.xmlId == client2_patient.xmlId, "could not transfer the patient maintaining the ID"
       end
 
-      # can add any function I need here to help with tests 
-      # Util type functions
 
     end
   end

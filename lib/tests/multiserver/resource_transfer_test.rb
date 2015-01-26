d789c6cfc8f0de27afa3d42762c1852532b8c83b
Evana13G <Evana13G@gmail.com>
2015-01-26 13:27:10 -0500

    finished tests

--- lib/tests/multiserver/resource_transfer_test.rb
+++ lib/tests/multiserver/resource_transfer_test.rb
@@ -7,7 +7,6 @@ module Crucible
       attr_accessor :client1_bundle
       attr_accessor :client2_bundle      
 
-
       attr_accessor :temp_resource
       attr_accessor :temp_id
       attr_accessor :temp_version
@@ -49,8 +48,6 @@ module Crucible
         end
       end
 
-
-
       test 'RT01','Transfer existing resource from client 1 to client 2' do
         metadata {
           links 'http://www.hl7.org/implement/standards/fhir/http.html#read'
@@ -74,28 +71,79 @@ module Crucible
           raise AssertionException.new("Preexisting #{resource_class.name.demodulize} on client 1 unknown.", nil)
         end
 
+        # use for resource comparision later
         client1_reply = @client.read(@resource_class, @preexisting_id)
 
-        if !client1_reply.code==201
-          raise AssertionException.new("Unable to read resource from service 2.")
-        end
+        # make sure the resource was read
+        assert_response_ok client1_reply, "Unable to read resource from service 1."
 
         @preexisting = client1_reply.resource
 
-        if @preexisting.nil?
+        if !@preexisting.nil?
           raise AssertionException.new("Failed to read preexisting #{resource_class.name.demodulize}: #{@preexisting_id} from client 1", client1_reply.body)
         end
 
         # create on client 2
+        # this might fail if you dont drop the id
         client2_reply = @client2.create @preexisting
-        if client2_reply.code==201
-          result.update(STATUS[:pass], "Preexisting resource #{resource_class.name.demodulize} was read from client 1 and created on client 2", client2_reply.body)
+
+        assert_response_ok client2_reply, "Failed to create #{resource_class.name.demodulize}: #{@preexisting_id} on client 2"
+
+        client2_resource_read = client2.read(client2_reply.id)
+
+        assert_response_ok client2_resource_read, "Failed to read #{resource_class.name.demodulize}: #{@preexisting_id} on client 2"
+
+        assert client2_resource_read.resource.equals?(@preexisting), "Found differences:"
+      end
+
+      test 'RT01a','Transfer existing resource from client 1 to client 2 - without id' do
+        metadata {
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#read'
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#create'
+          requires resource: @resource_class, methods: ["create", "read"]
+          validates resource: @resource_class, methods: ["create", "read"]
+        }
+
+        result = TestResult.new('RT01a',"Read #{resource_class.name.demodulize} from client 1 and create on client 2 - without id", nil, nil, nil)
+
+        client1_resource_reply = @client.read_feed(@resource_class)
+        @client1_bundle = client1_resource_reply.resource
+        assert !@client1_bundle.nil?, 'Service 1 did not respond with bundle.'
+        # ^ should I be using a bundle here to read an existing resource?
+
+        if !@client1_bundle.nil? && @client1_bundle.total>0 && !@client1_bundle.entry[0].nil? && !@client1_bundle.entry[0].resource.nil?
+          @preexisting_id = @client1_bundle.entry[0].resource.xmlId
+        elsif !@temp_resource.nil?
+          @preexisting_id = @temp_id
         else
-          outcome = self.parse_operation_outcome(client2_reply.body)
-          message = self.build_messages(outcome)
-          result.update(STATUS[:fail], message, client2_reply.body)
-          @temp_resource = nil
+          raise AssertionException.new("Preexisting #{resource_class.name.demodulize} on client 1 unknown.", nil)
         end
+
+        # use for resource comparision later
+        client1_reply = @client.read(@resource_class, @preexisting_id)
+
+        # make sure the resource was read
+        assert_response_ok client1_reply, "Unable to read resource from service 1."
+
+        @preexisting = client1_reply.resource
+
+        if !@preexisting.nil?
+          raise AssertionException.new("Failed to read preexisting #{resource_class.name.demodulize}: #{@preexisting_id} from client 1", client1_reply.body)
+        end
+
+        @preexisting.id = nil 
+
+        # create on client 2
+        # this might fail if you dont drop the id
+        client2_reply = @client2.create @preexisting
+
+        assert_response_ok client2_reply, "Failed to create #{resource_class.name.demodulize}: #{@preexisting_id} on client 2"
+
+        client2_resource_read = client2.read(client2_reply.id)
+
+        assert_response_ok client2_resource_read, "Failed to read #{resource_class.name.demodulize}: #{@preexisting_id} on client 2"
+
+        assert client2_resource_read.resource.equals?(@preexisting), "Found differences:"
       end
 
       test 'RT02','Transfer existing resource from client 2 to client 1' do
@@ -106,43 +154,277 @@ module Crucible
           validates resource: @resource_class, methods: ["create", "read"]
         }
 
-        result = TestResult.new('RT01',"Read #{resource_class.name.demodulize} from client 1 and create on client 2", nil, nil, nil)
+        result = TestResult.new('RT02',"Read #{resource_class.name.demodulize} from client 2 and create on client 1", nil, nil, nil)
 
-        client2_resource_reply = @client2.read_feed(@resource_class)
+        client2_resource_reply = @client.read_feed(@resource_class)
         @client2_bundle = client2_resource_reply.resource
         assert !@client2_bundle.nil?, 'Service 2 did not respond with bundle.'
         # ^ should I be using a bundle here to read an existing resource?
 
         if !@client2_bundle.nil? && @client2_bundle.total>0 && !@client2_bundle.entry[0].nil? && !@client2_bundle.entry[0].resource.nil?
-          @preexisting_id = @client1_bundle.entry[0].resource.xmlId
+          @preexisting_id = @client2_bundle.entry[0].resource.xmlId
         elsif !@temp_resource.nil?
           @preexisting_id = @temp_id
         else
           raise AssertionException.new("Preexisting #{resource_class.name.demodulize} on client 2 unknown.", nil)
         end
 
+        # use for resource comparision later
         client2_reply = @client2.read(@resource_class, @preexisting_id)
 
-        if !client2_reply.code==201
-          raise AssertionException.new("Unable to read resource from service 2.")
-        end
+        # make sure the resource was read
+        assert_response_ok client2_reply, "Unable to read resource #{resource_class.name.demodulize} from service 2."
 
         @preexisting = client2_reply.resource
 
-        if @preexisting.nil?
+        if !@preexisting.nil?
           raise AssertionException.new("Failed to read preexisting #{resource_class.name.demodulize}: #{@preexisting_id} from client 2", client2_reply.body)
         end
 
         # create on client 2
+        # this might fail if you dont drop the id
         client1_reply = @client.create @preexisting
-        if client1_reply.code==201
-          result.update(STATUS[:pass], "Preexisting resource #{resource_class.name.demodulize} was read from client 2 and created on client 1", client1_reply.body)
+
+        assert_response_ok client1_reply, "Failed to create #{resource_class.name.demodulize}: #{@preexisting_id} on client 1"
+
+        client1_resource_read = client1.read(client1_reply.id)
+
+        assert_response_ok client1_resource_read, "Failed to read #{resource_class.name.demodulize}: #{@preexisting_id} on client 1"
+
+        assert client1_resource_read.resource.equals?(@preexisting), "Found differences:"
+      end
+
+      test 'RT02a','Transfer existing resource from client 2 to client 1 - without id' do
+        metadata {
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#read'
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#create'
+          requires resource: @resource_class, methods: ["create", "read"]
+          validates resource: @resource_class, methods: ["create", "read"]
+        }
+
+        result = TestResult.new('RT02',"Read #{resource_class.name.demodulize} from client 2 and create on client 1 - without id", nil, nil, nil)
+
+        client2_resource_reply = @client.read_feed(@resource_class)
+        @client2_bundle = client2_resource_reply.resource
+        assert !@client2_bundle.nil?, 'Service 2 did not respond with bundle.'
+        # ^ should I be using a bundle here to read an existing resource?
+
+        if !@client2_bundle.nil? && @client2_bundle.total>0 && !@client2_bundle.entry[0].nil? && !@client2_bundle.entry[0].resource.nil?
+          @preexisting_id = @client2_bundle.entry[0].resource.xmlId
+        elsif !@temp_resource.nil?
+          @preexisting_id = @temp_id
         else
-          outcome = self.parse_operation_outcome(client1_reply.body)
-          message = self.build_messages(outcome)
-          result.update(STATUS[:fail], message, client1_reply.body)
-          @temp_resource = nil
+          raise AssertionException.new("Preexisting #{resource_class.name.demodulize} on client 2 unknown.", nil)
         end
+
+        # use for resource comparision later
+        client2_reply = @client2.read(@resource_class, @preexisting_id)
+
+        # make sure the resource was read
+        assert_response_ok client2_reply, "Unable to read resource #{resource_class.name.demodulize} from service 2."
+
+        @preexisting = client2_reply.resource
+
+        if !@preexisting.nil?
+          raise AssertionException.new("Failed to read preexisting #{resource_class.name.demodulize}: #{@preexisting_id} from client 2", client2_reply.body)
+        end
+
+        @preexisting.id = nil 
+
+        # create on client 2
+        # this might fail if you dont drop the id
+        client1_reply = @client.create @preexisting
+
+        assert_response_ok client1_reply, "Failed to create #{resource_class.name.demodulize}: #{@preexisting_id} on client 1"
+
+        client1_resource_read = client1.read(client1_reply.id)
+
+        assert_response_ok client1_resource_read, "Failed to read #{resource_class.name.demodulize}: #{@preexisting_id} on client 1"
+
+        assert client1_resource_read.resource.equals?(@preexisting), "Found differences:"
+      end
+
+
+      test 'RT03','Create resource on client 1 and transfer to client 2' do
+        metadata {
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#read'
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#create'
+          requires resource: @resource_class, methods: ["create", "read"]
+          validates resource: @resource_class, methods: ["create", "read"]
+        }
+
+        result = TestResult.new('RT03',"Create #{resource_class.name.demodulize} on client 1 and transfer to client 2", nil, nil, nil)
+
+        @temp_resource = ResourceGenerator.generate(@resource_class,3)
+
+        client1_reply = @client.create @temp_resource
+
+        assert_response_ok client1_reply, "Unable to create resouruce #{resource_class.name.demodulize} on client 1"
+
+        @temp_id = client1_reply.id
+        @temp_version = client1_reply.version
+
+        # use for resource comparision later
+        client1_reply = @client.read(@resource_class, @temp_id)
+
+        # make sure the resource was read
+        assert_response_ok client1_reply, "Unable to read resource from service 1."
+
+        @preexisting = client1_reply.resource
+
+        if !@preexisting.nil?
+          raise AssertionException.new("Failed to read preexisting #{resource_class.name.demodulize}: #{@preexisting_id} from client 1", client1_reply.body)
+        end
+
+        # create on client 2
+        # this might fail if you dont drop the id
+        client2_reply = @client2.create @preexisting
+
+        assert_response_ok client2_reply, "Failed to create #{resource_class.name.demodulize}: #{@preexisting_id} on client 2"
+
+        client2_resource_read = client2.read(client2_reply.id)
+
+        assert_response_ok client2_resource_read, "Failed to read #{resource_class.name.demodulize}: #{@preexisting_id} on client 2"
+
+        assert client2_resource_read.resource.equals?(@preexisting), "Found differences:"
+      end
+
+      test 'RT03a','Create resource on client 1 and transfer to client 2 - without id' do
+        metadata {
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#read'
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#create'
+          requires resource: @resource_class, methods: ["create", "read"]
+          validates resource: @resource_class, methods: ["create", "read"]
+        }
+
+        result = TestResult.new('RT03a',"Create #{resource_class.name.demodulize} on client 1 and transfer to client 2", nil, nil, nil)
+
+        @temp_resource = ResourceGenerator.generate(@resource_class,3)
+
+        client1_reply = @client.create @temp_resource
+
+        assert_response_ok client1_reply, "Unable to create resouruce #{resource_class.name.demodulize} on client 1"
+
+        @temp_id = client1_reply.id
+        @temp_version = client1_reply.version
+
+        # use for resource comparision later
+        client1_reply = @client.read(@resource_class, @temp_id)
+
+        # make sure the resource was read
+        assert_response_ok client1_reply, "Unable to read resource from service 1."
+
+        @preexisting = client1_reply.resource
+
+        if !@preexisting.nil?
+          raise AssertionException.new("Failed to read preexisting #{resource_class.name.demodulize}: #{@preexisting_id} from client 1", client1_reply.body)
+        end
+
+        @preexisting.id = nil 
+
+        # create on client 2
+        # this might fail if you dont drop the id
+        client2_reply = @client2.create @preexisting
+
+        assert_response_ok client2_reply, "Failed to create #{resource_class.name.demodulize}: #{@preexisting_id} on client 2"
+
+        client2_resource_read = client2.read(client2_reply.id)
+
+        assert_response_ok client2_resource_read, "Failed to read #{resource_class.name.demodulize}: #{@preexisting_id} on client 2"
+
+        assert client2_resource_read.resource.equals?(@preexisting), "Found differences:"
+      end
+
+
+      test 'RT04','Create resource on client 2 and transfer to client 1' do
+        metadata {
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#read'
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#create'
+          requires resource: @resource_class, methods: ["create", "read"]
+          validates resource: @resource_class, methods: ["create", "read"]
+        }
+
+        result = TestResult.new('RT04',"Create #{resource_class.name.demodulize} on client 2 and transfer to client 1", nil, nil, nil)
+
+        @temp_resource = ResourceGenerator.generate(@resource_class,3)
+
+        client2_reply = @client.create @temp_resource
+
+        assert_response_ok client2_reply, "Unable to create resouruce #{resource_class.name.demodulize} on client 2"
+
+        @temp_id = client2_reply.id
+        @temp_version = client2_reply.version
+
+        # use for resource comparision later
+        client2_reply = @client2.read(@resource_class, @temp_id)
+
+        # make sure the resource was read
+        assert_response_ok client2_reply, "Unable to read resource from service 2."
+
+        @preexisting = client2_reply.resource
+
+        if !@preexisting.nil?
+          raise AssertionException.new("Failed to read preexisting #{resource_class.name.demodulize}: #{@preexisting_id} from client 2", client2_reply.body)
+        end
+
+        # create on client 2
+        # this might fail if you dont drop the id
+        client1_reply = @client.create @preexisting
+
+        assert_response_ok client1_reply, "Failed to create #{resource_class.name.demodulize}: #{@preexisting_id} on client 1"
+
+        client1_resource_read = @client.read(client1_reply.id)
+
+        assert_response_ok client1_resource_read, "Failed to read #{resource_class.name.demodulize}: #{@preexisting_id} on client 1"
+
+        assert client1_resource_read.resource.equals?(@preexisting), "Found differences:"
+      end
+
+
+      test 'RT04a','Create resource on client 2 and transfer to client 1 - without id' do
+        metadata {
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#read'
+          links 'http://www.hl7.org/implement/standards/fhir/http.html#create'
+          requires resource: @resource_class, methods: ["create", "read"]
+          validates resource: @resource_class, methods: ["create", "read"]
+        }
+
+        result = TestResult.new('RT04a',"Create #{resource_class.name.demodulize} on client 2 and transfer to client 1 - without id", nil, nil, nil)
+
+        @temp_resource = ResourceGenerator.generate(@resource_class,3)
+
+        client2_reply = @client.create @temp_resource
+
+        assert_response_ok client2_reply, "Unable to create resouruce #{resource_class.name.demodulize} on client 2"
+
+        @temp_id = client2_reply.id
+        @temp_version = client2_reply.version
+
+        # use for resource comparision later
+        client2_reply = @client2.read(@resource_class, @temp_id)
+
+        # make sure the resource was read
+        assert_response_ok client2_reply, "Unable to read resource from service 2."
+
+        @preexisting = client2_reply.resource
+
+        if !@preexisting.nil?
+          raise AssertionException.new("Failed to read preexisting #{resource_class.name.demodulize}: #{@preexisting_id} from client 2", client2_reply.body)
+        end
+
+        @preexisting.id = nil         
+
+        # create on client 2
+        # this might fail if you dont drop the id
+        client1_reply = @client.create @preexisting
+
+        assert_response_ok client1_reply, "Failed to create #{resource_class.name.demodulize}: #{@preexisting_id} on client 1"
+
+        client1_resource_read = @client.read(client1_reply.id)
+
+        assert_response_ok client1_resource_read, "Failed to read #{resource_class.name.demodulize}: #{@preexisting_id} on client 1"
+
+        assert client1_resource_read.resource.equals?(@preexisting), "Found differences:"
       end
 
     end

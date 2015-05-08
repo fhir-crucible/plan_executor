1140ec2442fc8b0db7a482ea247937afb283b3cc
Janoo Fernandes <smoothclip@gmail.com>
2015-05-08 14:38:08 -0400

    Update resource generator with more reasonable values

--- lib/resource_generator.rb
+++ lib/resource_generator.rb
@@ -20,14 +20,16 @@ module Crucible
         resource
       end
 
-      # 
+      #
       # Set the fields of this resource to have some random values.
       #
       def self.set_fields!(resource)
         fields = resource.fields
         fields.each do |key,value|
           type = value.options[:type]
-		  next if key=='id' || key=='xmlId'
+		      next if key=='id' || key=='xmlId'
+          # FIXME: Remove below once dateTime fields are fixed in DSTU2 models
+          next if ['scheduledTime', 'time', 'effectiveTime', 'authoringTime', 'receivedTime'].include? key
           gen = nil
           if type == String
             gen = SecureRandom.urlsafe_base64
@@ -41,7 +43,7 @@ module Crucible
           elsif type == Integer
             gen = SecureRandom.random_number(100)
           elsif type == Float
-            gen = SecureRandom.random_number          
+            gen = SecureRandom.random_number
           elsif type == Mongoid::Boolean
             gen = (SecureRandom.random_number(100) % 2 == 0)
           elsif type == FHIR::PartialDateTime
@@ -56,18 +58,23 @@ module Crucible
           # else
           #   puts "Unabled to generate field #{key} for #{resource.class} -- unrecognized type: #{type}"
           end
+          # TODO: Improve field value generation for coded fields
+          gen = gen[0..19] if key == 'language'
           resource[key] = gen if !gen.nil?
         end
         resource
       end
 
 
-      # 
+      #
       # Generate children for this resource.
       #
       def self.generate_children!(resource,embedded=0)
         children = resource.embedded_relations
         children.each do |key,value|
+          # TODO: Determine if we can generate references or meta information
+          next if ['meta'].include? key
+          next if value[:class_name] == 'FHIR::Reference'
           klass = resource.get_fhir_class_from_resource_type(value[:class_name])
           child = generate(klass,(embedded-1)) if(value[:class_name] != 'FHIR::Extension')
           if value[:relation] == Mongoid::Relations::Embedded::Many

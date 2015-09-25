b9e74a7320275aacc6132a066005c71400ae6323
Jason Walonoski <jwalonoski@mitre.org>
2015-09-25 11:12:51 -0400

    Update history tests.

--- lib/tests/suites/history_test.rb
+++ lib/tests/suites/history_test.rb
@@ -18,20 +18,22 @@ module Crucible
 
         @version = []
         result = @client.create(@patient)
+        assert_response_ok(result)
         @id = result.id
         @version << result.version
 
+        @patient.xmlId = @id
         @patient.telecom << FHIR::ContactPoint.new(system: 'email', value: 'foo@example.com')
 
         update_result = @client.update(@patient, @id)
+        assert_response_ok(update_result)
         @version << update_result.version
-
-        @client.destroy(FHIR::Patient, @id)
-
+        del_result = @client.destroy(FHIR::Patient, @id)
+        assert_response_code(del_result,204)
+        
         @entry_count = @version.length
         # add one for deletion
         @version_count = @entry_count + 1
-
       end
 
       test  'HI01','History for specific resource' do
@@ -50,7 +52,7 @@ module Crucible
         check_sort_order(bundle.entry)
       end
 
-      test  'HI01.1','History transactions entries' do
+      test  'HI01.1','History request entries' do
         metadata {
           links "#{REST_SPEC_LINK}#history"
           requires resource: "Patient", methods: ["create", "update", "delete"]
@@ -61,9 +63,9 @@ module Crucible
         bundle = result.resource
         entries = bundle.entry
 
-        assert_equal 1, entries.select{|entry| entry.transaction.try(:method) == "DELETE" }, "Could not find a deleted transation on entry in the history bundle"
-        assert_equal 1, entries.select{|entry| entry.transaction.try(:method) == "PUT" }, "Could not find a deleted transation on entry in the history bundle"
-        assert_equal 1, entries.select{|entry| entry.transaction.try(:method) == "POST" }, "Could not find a deleted transation on entry in the history bundle"
+        assert_equal 1, entries.select{|entry| entry.request.try(:method) == 'DELETE' }.size, 'Wrong number of DELETE transactions in the history bundle'
+        assert_equal 1, entries.select{|entry| entry.request.try(:method) == 'PUT' }.size, 'Wrong number of PUT transactions in the history bundle'
+        assert_equal 1, entries.select{|entry| entry.request.try(:method) == 'POST' }.size, 'Wrong number of POST transactions in the history bundle'
 
       end
 
@@ -112,7 +114,7 @@ module Crucible
         end
 
         deleted_entries(bundle.entry).each do |entry|
-          # FIXME: Should we parse the transaction URL or drop this assertion?
+          # FIXME: Should we parse the request URL or drop this assertion?
           if entry.resource
             pulled = @client.vread(FHIR::Patient, entry.resource.xmlId, entry.resource.meta.versionId)
             assert pulled.resource.nil?, "resource should not be found since it was deleted"
@@ -189,7 +191,7 @@ module Crucible
           validates resource: "Patient", methods: ["history-type"]
         }
 
-        page_size = 30
+        page_size = 2
         page = @client.history(resource: FHIR::Patient, history: {since: (Time.now.utc - 1.hour), count: page_size})
 
         forward_count = 0
@@ -212,11 +214,12 @@ module Crucible
           validates resource: "Patient", methods: ["history-type"]
         }
 
-        page_size = 30
+        page_size = 2
         page = @client.history(resource: FHIR::Patient, history: {since: (Time.now.utc - 1.hour), count: page_size})
 
         forward_count = 0
         last_page = page
+        # browse forwards
         while page != nil
           assert !page.resource.nil?, "Unable to page forward through results.  A bundle was not returned from the page forward request."
           forward_count += page.resource.entry.size
@@ -226,7 +229,7 @@ module Crucible
 
         backward_count = 0
         page = last_page
-        # browse forwards
+        # browse backwards
         while page != nil
           warning { entry_ids_are_present(page.resource.entry) }
           assert page.resource.entry.size <= page_size, "Server returned a page with more entries than set by _count"
@@ -247,7 +250,7 @@ module Crucible
 
         history = @client.all_history
         assert !history.resource.nil?, "A bundle was not returned from the history request."
-        assert history.resource.entry.size > 2, "there should be at least 2 history entries"
+        assert history.resource.entry.size >= 3, "there should be at least 3 history entries"
       end
 
       ###
@@ -261,8 +264,8 @@ module Crucible
 
       def deleted_entries(entries)
         entries.select do |entry|
-          assert !entry.transaction.nil?, "history bundle entries do not have transaction elements, deleted entries cannot be distinguished"
-          entry.transaction.try(:method) == "DELETE"
+          assert !entry.request.nil?, "history bundle entries do not have request elements, deleted entries cannot be distinguished"
+          entry.request.try(:method) == "DELETE"
         end
       end
 
@@ -272,10 +275,11 @@ module Crucible
 
 
       def entry_ids_are_present(entries)
-        ids = entries.map(&:resource).map(&:xmlId).compact rescue assert(false, "could not find ids for resources returned by the bundle")
+        relevant_entries = entries.select{|x|x.request.try(:method)!='DELETE'}
+        ids = relevant_entries.map(&:resource).map(&:xmlId).compact rescue assert(false, 'Unable to find IDs for resources returned by the bundle')
 
         # check that we have ids and self links
-        assert_equal entries.length, ids.size, "all of the returned entries must have an id"
+        assert_equal relevant_entries.length, ids.size, 'All PUT and POST entries must have an ID'
       end
 
       def url?(v)
@@ -283,14 +287,14 @@ module Crucible
       end
 
       def check_sort_order(entries)
-        entries.each_cons(2) do |left, right|
-          assert !left.resource.meta.nil?, 'Unable to determine if entries are in the correct order -- no meta'
-          assert !right.resource.meta.nil?, 'Unable to determine if entries are in the correct order -- no meta'
+        relevant_entries = entries.select{|x|x.request.try(:method)!='DELETE'}
+        relevant_entries.map!(&:resource).map!(&:meta).compact rescue assert(false, 'Unable to find meta for resources returned by the bundle')
 
-          if !left.resource.meta.versionId.nil? && !right.resource.meta.versionId.nil?
-            assert (left.resource.meta.versionId > right.resource.meta.versionId), 'Result contains entries in the wrong order.'
-          elsif !left.resource.meta.lastUpdated.nil? && !right.resource.meta.lastUpdated.nil?
-            assert (left.resource.meta.lastUpdated >= right.resource.meta.lastUpdated), 'Result contains entries in the wrong order.'
+        relevant_entries.each_cons(2) do |left, right|
+          if !left.versionId.nil? && !right.versionId.nil?
+            assert (left.versionId > right.versionId), 'Result contains entries in the wrong order.'
+          elsif !left.lastUpdated.nil? && !right.lastUpdated.nil?
+            assert (left.lastUpdated >= right.lastUpdated), 'Result contains entries in the wrong order.'
           else
             raise AssertionException.new 'Unable to determine if entries are in the correct order -- no meta.versionId or meta.lastUpdated'
           end

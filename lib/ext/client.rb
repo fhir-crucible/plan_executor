9c58e8fd38c489cb50240882b7203464b347374d
Andre Quina <andre.quina@gmail.com>
2016-04-11 20:49:37 -0400

    uncomment code that stores request/responses... works with client fixes

--- lib/ext/client.rb
+++ lib/ext/client.rb
@@ -13,16 +13,16 @@ module FHIR
       return if @decorated
       @decorated = true
       # TODO: RESPONSES... FIX THIS!!!!
-      # [:get, :put, :post, :delete, :head].each do |method|
-      #   class_eval %Q{
-      #     alias #{method}_original #{method}
-      #     def #{method}(*args, &block)
-      #       reply = #{method}_original(*args, &block)
-      #       record_requests(reply)
-      #       return reply
-      #     end
-      #   }
-      # end
+      [:get, :put, :post, :delete, :head].each do |method|
+        class_eval %Q{
+          alias #{method}_original #{method}
+          def #{method}(*args, &block)
+            reply = #{method}_original(*args, &block)
+            record_requests(reply)
+            return reply
+          end
+        }
+      end
     end
   end
 end

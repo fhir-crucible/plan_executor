4c851b541a3a864590169490ef8b63f552382e82
Michael O'Keefe <mokeefe@mitre.org>
2016-08-08 12:54:37 -0400

    Implemented first scenario (plus a test to get a provider with roles)

--- lib/tests/suites/argonaut_provider_connectathon_test.rb
+++ lib/tests/suites/argonaut_provider_connectathon_test.rb
@@ -14,11 +14,53 @@ module Crucible
       end
 
       def setup
-
+        @tags.append('provider')
+        @category = {id: 'argonaut', title: 'Argonaut'}
       end
 
-      test 'APCT01', 'Test Ability to locate a Practitioner\'s Telecom/Physical Address' do
-        
+      test 'APCT01', 'GET A set of practitioners to test' do
+        metadata {
+          links "#{REST_SPEC_LINK}#read"
+          links "#{REST_SPEC_LINK}#search"
+          requires resource: 'Practitioner', methods: ['read', 'search']
+          validates resource: 'Practitioner', methods: ['read', 'search']
+        }
+
+        # Basically just get a group of 10 Practitioners
+        options = {
+            :search => {
+              :flag => true,
+              :compartment => nil,
+              :parameters => {
+                _count: 100
+              }
+            }
+          }
+          @practitioners = @client.search(FHIR::Practitioner, options).resource.try(:entry)
+          assert @practitioners, 'No Practitioners found'
+          @practitioner_id = @practitioners.select{ |p| !p.resource.practitionerRole.empty? }.sample.try(:resource).try(:id)
+          assert @practitioner_id, 'No practitioner found with a PractitionerRole'
+      end
+
+      test 'APCT02', 'Test Ability to locate a Practitioner\'s Telecom/Physical Address' do
+        metadata {
+          links "#{REST_SPEC_LINK}#read"
+          requires resource: 'Practitioner', methods: ['read']
+          validates resource: 'Practitioner', methods: ['read']
+        }
+
+        skip if !@practitioner_id
+
+        @practitioner = @client.read(FHIR::Practitioner, @practitioner_id).try(:resource)
+
+        assert @practitioner, "No Practitioner found for ID #{@practitioner_id}"
+
+        assert @practitioner.practitionerRole.select{ |pr| !pr.location.empty? }.size >= 1, "No Locations found for Practitioner #{@practitioner.identifier}"
+
+        assert @practitioner.practitionerRole.select{ |pr| !pr.location.select {|loc| loc.address != nil }.empty? }.size >= 1, "No addresses found for Practitioner #{@practitioner.identifier}"
+
+        assert @practitioner.practitionerRole.select{ |pr| !pr.telecom.empty? }.size >= 1, "No telecoms found for Practitioner #{@practitioner.identifier}"
+
       end
     end
   end

f68eefc80dc475fac1f684ac8a0bf1a5dc5af08b
Jason Walonoski <jwalonoski@mitre.org>
2016-10-25 15:53:00 -0400

    Remove useless client requests from robust search test.

--- lib/tests/suites/search_test_robust.rb
+++ lib/tests/suites/search_test_robust.rb
@@ -16,75 +16,16 @@ module Crucible
       end
 
       def setup
-        # Create a patient with gender:missing
+        # Create a patient
         patient = Crucible::Generator::Resources.new.minimal_patient
         patient.identifier = [FHIR::Identifier.new]
         patient.identifier[0].value = SecureRandom.urlsafe_base64
-
         ignore_client_exception { @patient = FHIR::Patient.create(patient) }
-
         assert @patient, "Response code #{@client.reply.code} on patient creation."
-
-        # read all the patients
-        @patients = FHIR::Patient.all()
-
-        # create a condition matching the first patient
-        condition = ResourceGenerator.generate(FHIR::Condition,1)
-
-        # remove common constraint issues for conditions
-        # these fields don't affect the test so no need to have them in the resource that we are using
-        # but if they are technically malformed some servers will reject them, and cause us to skip the whole test
-        condition.evidence = nil
-        condition.stage = nil
-        condition.abatementAge = nil
-        condition.onsetAge = nil
-
-        condition.subject = ResourceGenerator.generate(FHIR::Reference)
-
-        condition.subject.id = @patients.entry.first.resource.id
-        options = {
-          :id => condition.subject.id,
-          :resource => @patients.entry.first.resource.class
-        }
-        condition.subject.reference = @client.resource_url(options)
-
-        ignore_client_exception { @condition = FHIR::Condition.create(condition) }
-
-        assert @condition, "Response code #{@client.reply.code} on condition creation."
-
-        # create some observations
-        @observations = [4.12345, 4.12346, 4.12349, 5.12, 6.12].map {|n| create_observation(n)}
-      end
-
-      def create_observation(value)
-        observation = FHIR::Observation.new
-        observation.status = 'preliminary'
-        code = FHIR::Coding.new
-        code.system = 'http://loinc.org'
-        code.code = '2164-2'
-        observation.code = FHIR::CodeableConcept.new
-        observation.code.coding = [ code ]
-        observation.valueQuantity = FHIR::Quantity.new
-        observation.valueQuantity.system = 'http://unitofmeasure.org'
-        observation.valueQuantity.value = value
-        observation.valueQuantity.unit = 'mmol'
-        body = FHIR::Coding.new
-        body.system = 'http://snomed.info/sct'
-        body.code = '182756003'
-        observation.bodySite = FHIR::CodeableConcept.new
-        observation.bodySite.coding = [ body ]
-        begin
-          result = FHIR::Observation.create(observation) rescue nil
-        rescue ClientException=>e
-        end
-        assert result, "Response code #{@client.reply.code} on observation creation."
-        result
       end
 
       def teardown
         ignore_client_exception { @patient.destroy }
-        ignore_client_exception { @condition.destroy }
-        @observations.each {|o| ignore_client_exception { o.destroy } }
       end
 
     [true,false].each do |flag|  

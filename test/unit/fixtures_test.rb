26475348ddfb95932574401df81803547e7f3bf1
Jason Walonoski <jwalonoski@mitre.org>
2016-12-12 15:22:41 -0500

    Update fixture loading and fixture unit tests.

--- test/unit/fixtures_test.rb
+++ test/unit/fixtures_test.rb
@@ -2,7 +2,13 @@ require_relative '../test_helper'
 
 class FixturesTest < Test::Unit::TestCase
 
+  ERROR_DIR = File.join('tmp', 'errors', 'FixturesTest')
+  # Create a blank folder for the errors
+  FileUtils.rm_rf(ERROR_DIR) if File.directory?(ERROR_DIR)
+  FileUtils.mkdir_p ERROR_DIR
+
   fixtures = File.join('fixtures','**','*.xml')
+  raise 'No Fixture Files Found' if Dir[fixtures].empty?
 
   # Define test methods to validate example JSON
   Dir.glob(fixtures).each do | file |    
@@ -11,18 +17,28 @@ class FixturesTest < Test::Unit::TestCase
     xml = File.open(file, 'r:bom|UTF-8', &:read)
 
     define_method("test_fixture_validation_#{basename}") do
-      run_validate(file,xml)
+      run_validate(basename,xml)
     end
   end
 
-  def run_validate(file,xml)
-    assert(file && xml)
+  def run_validate(fixture,xml)
+    assert(fixture && xml)
 
     r = FHIR::Xml.from_xml(xml)
     assert(!r.nil?,"XML fixture does not deserialize.")
     
-    valid = FHIR::Xml.is_valid?(xml)
-    assert(valid,"XML fixture does not conform to schema.")
+    errors = FHIR::Xml.validate(xml)
+    if !errors.empty?
+      File.open("#{ERROR_DIR}/#{fixture}.err", 'w:UTF-8') do |file|
+        file.write "#{fixture}: #{errors.length} errors\n\n"
+        errors.each do |error|
+          file.write(sprintf("%-8d  %s\n", error.line, error.message))
+        end
+      end
+      File.open("#{ERROR_DIR}/#{fixture}.xml", 'w:UTF-8') { |file| file.write(xml) }
+    end
+
+    assert(errors.empty?,"XML fixture does not conform to schema.")
     # TODO Add StructureDefinition validation.
   end
 

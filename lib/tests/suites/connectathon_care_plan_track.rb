a33c8518b132ce27f26ef92177d4e4fa461dc1ff
Michael O'Keefe <mokeefe@mitre.org>
2017-01-10 16:27:27 -0500

    Added test 3 to careplan suite

--- lib/tests/suites/connectathon_care_plan_track.rb
+++ lib/tests/suites/connectathon_care_plan_track.rb
@@ -11,31 +11,44 @@ module Crucible
       end
 
       def setup
-        @resources = Crucible::Tests::ResourceGenerator
-        @records = {}
+        begin
+          @resources = Crucible::Tests::ResourceGenerator
+          @records = {}
 
-        patient = @resources.generate(FHIR::Patient, 3)
-        create_object(patient, :patient)
+          patient = @resources.generate(FHIR::Patient, 3)
+          create_object(patient, "patient")
 
-        @num_care_plans = rand(5..10)
+          @num_care_plans = rand(5..10)
 
-        @num_care_plans.times do |t|
-          care_plan = @resources.generate(FHIR::CarePlan, 3)
-          care_plan.subject = patient.to_reference
-          care_plan.category = []
-          create_object(care_plan, "care_plan_#{t}")
-        end
+          # Create some generic care plans, with a subject and no categories
+          @num_care_plans.times do |t|
+            care_plan = @resources.generate(FHIR::CarePlan, 3)
+            care_plan.subject = patient.to_reference
+            care_plan.category = []
+            create_object(care_plan, "care_plan_#{t}")
+          end
 
-        @num_cancer_care_plans = rand(1..5)
-        @cancer_care_plan_category = FHIR::CodeableConcept.new
-        @cancer_care_plan_category.coding = FHIR::Coding.new(code: "395082007", system: "http://snomed.info/sct")
-        @num_cancer_care_plans.times do |t|
-          care_plan = @resources.generate(FHIR::CarePlan, 3)
-          care_plan.subject = patient.to_reference
-          care_plan.category = [@cancer_care_plan_category]
-          # require 'pry'
-          # binding.pry
-          create_object(care_plan, "cancer_care_plan_#{t}")
+          # Create some cancer-category care plans
+          @num_cancer_care_plans = rand(1..5)
+          @cancer_care_plan_category = FHIR::CodeableConcept.new(text: "Cancer care plan")
+          @cancer_care_plan_category.coding = FHIR::Coding.new(code: "395082007", system: "http://snomed.info/sct", display: "Cancer care plan")
+          @num_cancer_care_plans.times do |t|
+            care_plan = @resources.generate(FHIR::CarePlan, 3)
+            care_plan.subject = patient.to_reference
+            care_plan.category = [@cancer_care_plan_category]
+            create_object(care_plan, "cancer_care_plan_#{t}")
+          end
+
+          # Create some care teams for a patient
+          @num_care_teams = rand(1..5)
+          @num_care_teams.times do |t|
+            care_team = @resources.generate(FHIR::CareTeam, 3)
+            care_team.subject = patient.to_reference
+            create_object(care_team, "care_team_#{t}")
+          end
+        rescue Exception => e
+          require 'pry'
+          binding.pry
         end
       end
 
@@ -65,7 +78,7 @@ module Crucible
             :flag => false,
             :compartment => nil,
             :parameters => {
-              'patient' => @records[:patient].id
+              'patient' => @records["patient"].id
             }
           }
         }
@@ -76,11 +89,11 @@ module Crucible
         assert_equal @num_care_plans + @num_cancer_care_plans, reply.resource.entry.count
 
         reply.resource.entry.each do |entry|
-          assert entry.resource.subject.equals? @records[:patient].to_reference, "care plan #{entry.resource.id} subject ID doesn't match given subject ID #{@records[:patient].to_reference}"
+          assert entry.resource.subject.equals?(@records["patient"].to_reference), "care plan #{entry.resource.id} subject ID doesn't match given subject ID #{@records["patient"].to_reference}"
         end
       end
 
-      test 'CCPT1','Search for all Cancer-category Care Plans for a Patient' do
+      test 'CCPT2','Search for all Cancer-category Care Plans for a Patient' do
         metadata {
           links "#{REST_SPEC_LINK}#search"
           links 'http://wiki.hl7.org/index.php?title=201701_Care_Plan#Search_for_all_Care_Plans_for_a_patient'
@@ -94,7 +107,7 @@ module Crucible
             :flag => false,
             :compartment => nil,
             :parameters => {
-              'patient' => @records[:patient].id,
+              'patient' => @records["patient"].id,
               'category' => '395082007'
             }
           }
@@ -106,8 +119,37 @@ module Crucible
         assert_equal @num_cancer_care_plans, reply.resource.entry.count
 
         reply.resource.entry.each do |entry|
-          assert entry.resource.subject.equals? @records[:patient].to_reference, "care plan #{entry.resource.id} subject ID doesn't match given subject ID #{@records[:patient].to_reference}"
-          assert entry.resource.category.first.equals? @cancer_care_plan_category, "care plan #{entry.resource.id} category code (entry.resource.category.first) doesn't match cancer category code #{@cancer_care_plan_category}"
+          assert entry.resource.subject.equals?(@records["patient"].to_reference), "care plan #{entry.resource.id} subject ID doesn't match given subject ID #{@records["patient"].to_reference}"
+          assert entry.resource.category.first.coding.first.equals?(@cancer_care_plan_category.coding), "care plan category code doesn't match cancer category code #{@cancer_care_plan_category} (#{entry.resource.category.first.coding.first.mismatch(@cancer_care_plan_category.coding)})"
+        end
+      end
+
+      test 'CCPT3','Search for all Care Teams for a Patient' do
+        metadata {
+          links "#{REST_SPEC_LINK}#search"
+          links 'http://wiki.hl7.org/index.php?title=201701_Care_Plan#Search_for_all_Care_Plans_for_a_patient'
+          requires resource: 'Patient', methods: ['create', 'read']
+          requires resource: 'CareTeam', methods: ['create', 'read']
+          validates resource: 'CareTeam', methods: ['create', 'read']
+        }
+
+        options = {
+          :search => {
+            :flag => false,
+            :compartment => nil,
+            :parameters => {
+              'patient' => @records["patient"].id
+            }
+          }
+        }
+
+        reply = @client.search(FHIR::CareTeam, options)
+        assert_response_ok(reply)
+
+        assert_equal @num_care_teams, reply.resource.entry.count
+
+        reply.resource.entry.each do |entry|
+          assert entry.resource.subject.equals?(@records["patient"].to_reference), "care team #{entry.resource.id} subject ID doesn't match given subject ID #{@records["patient"].to_reference}"
         end
       end
 

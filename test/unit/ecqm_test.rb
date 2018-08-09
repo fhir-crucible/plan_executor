cd9293fab6037a5ab7682b50b3c127583d6151a5
cmbear37 <christina_bear@college.harvard.edu>
2018-08-09 11:43:21 -0400

    initial measure testscript

---
+++ test/unit/ecqm_test.rb
@@ -0,0 +1,14 @@
+require_relative '../test_helper'
+
+class TestScriptECQMTest < Test::Unit::TestCase
+  def setup
+    # stub out methods
+  end
+
+
+  def test_my_testscript
+    # create a TestScriptEngine
+    # find my testScript
+    # execute it
+  end
+end

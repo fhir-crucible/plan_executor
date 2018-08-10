d1f7e203de246a88e2a9b6b32b000fc11e297df4
cmbear37 <christina_bear@college.harvard.edu>
2018-08-10 15:55:31 -0400

    documentation

--- test/unit/ecqm_test.rb
+++ test/unit/ecqm_test.rb
@@ -1,14 +1,20 @@
 require_relative '../test_helper'
+require 'webmock'
+
+# this is a unit test for running my testscript
 
 class TestScriptECQMTest < Test::Unit::TestCase
-  def setup
-    # stub out methods
+	include WebMock::API
+
+  def initialize(name = nil)
+   super(name)
   end
 
 
   def test_my_testscript
     # create a TestScriptEngine
     # find my testScript
+    # stub out and put all the expected things for each network call in the test script
     # execute it
   end
 end

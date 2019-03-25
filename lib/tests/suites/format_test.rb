769b689f4debb00431329778f11d83650955d4ad
Rob Scanlon <robscanlon@gmail.com>
2019-03-25 11:06:31 -0400

    Allow changes to versionId and lastUpdated.

--- lib/tests/suites/format_test.rb
+++ lib/tests/suites/format_test.rb
@@ -306,8 +306,12 @@ module Crucible
       private
 
       # Compare requested resource with created resource
+      # @resource is a fixture before it has been posted to the server.
+      # entry is what was returned from a read.
+      # The server is expected to update 'id' and add 'meta.lastUpdated',
+      # and if history is supported add meta.versionId.  Ignore any changes to these.
       def compare_response(entry)
-        !entry.nil? && !entry.resource.nil? && entry.resource.equals?(@resource,['id'])
+        entry&.resource&.equals?(@resource,['id','lastUpdated','versionId']) == true
       end
 
       # Compare response format with requested format
@@ -317,8 +321,7 @@ module Crucible
 
       # Compare two requested entries
       def compare_entries(entry1, entry2)
-        compare_response(entry1) && compare_response(entry2) && entry1.resource.equals?(entry2.resource,['id'])
-      end
+        compare_response(entry1) && compare_response(entry2) && entry1.resource.equals?(entry2.resource,['id']) end
 
       # Unify resource requests and format specification
       def request_entry(resource_class, id, format, use_format_param=false)
